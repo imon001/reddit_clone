b19f3622a6819505043f964db25a2c51f9234d21
imon001 <bishojitdasemon01@gmail.com>
2023-12-22 19:08:23 +0600

    bottom navbar added

---
+++ lib/features/feeds/screens/feeds_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FeedsScreen extends ConsumerWidget {
+  const FeedsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_two/core/constants/constants.dart';
 
+import '../../../theme/pallete.dart';
 import '../../auth/controllers/auth_controller.dart';
 import '../delegates/search_community_delegate.dart';
 import '../drawer/community_list_drawer.dart';
 import '../drawer/profile_drawer.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _page = 0;
   void displayDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
   }
@@ -17,9 +25,16 @@ class HomeScreen extends ConsumerWidget {
     Scaffold.of(context).openEndDrawer();
   }
 
+  void onPageChange(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final user = ref.watch(userProvider);
+    final currentTheme = ref.watch(themeNotifierProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -47,16 +62,27 @@ class HomeScreen extends ConsumerWidget {
                 backgroundImage: NetworkImage(user?.profilePic ?? Constants.avatarDefault),
               ),
             );
-          }),
+          })
         ],
       ),
       drawer: const CommunityListDrawer(),
       endDrawer: const ProfileDrawer(),
-      body: Center(
-        child: Text(
-          user?.karma.toString() ?? "0",
-          style: const TextStyle(color: Colors.white),
-        ),
+      body: Constants.tabWidgets[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: currentTheme.iconTheme.color,
+        backgroundColor: currentTheme.backgroundColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: '',
+          ),
+        ],
+        onTap: onPageChange,
+        currentIndex: _page,
       ),
     );
   }
